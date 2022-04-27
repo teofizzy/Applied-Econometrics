a7478b8289f8c172dd8816dc5639e447a6e8dffc
Teofilo Acholla Ligawa Gafna <teofilo48ligawa@gmail.com>
2022-04-27 23:48:55 +0300

    cointegration test

--- collegetown-slr.R
+++ collegetown-slr.R
@@ -27,3 +27,25 @@ ggplot(X1649256667723_collegetown, aes(x = sqft, y = price)) +
 library(lmtest)
 grangertest(price ~ sqft, order = 3, data = X1649256667723_collegetown)
 grangertest(sqft ~ price, order = 3, data = X1649256667723_collegetown)
+
+##cointegration test
+
+#step 1, run a regression
+reg_long_run<-lm(price~sqft, data = X1649256667723_collegetown)
+reg_long_run
+resid_reg_long_run<-resid(reg_long_run)
+
+#step2, conduct a stationarity test on the residuals
+library(urca)
+install.packages("urca")
+library(urca)
+#urca-unit root and cointegration package, df = dickey-fuller
+#use none because we have done a regresiion already, select lags = AIC
+y<-ur.df(resid_reg_long_run, type = "none", selectlags = "AIC")
+y
+summary(y)
+y@teststat
+y@cval
+#from the above, -7 is less than -1, thus we reject the null hypothesis of non-stationarity
+#|and conclude that the residuals are stationary which implies that these two variables are
+#|cointegrated whiich means that there exists some long-run relationship between them.
