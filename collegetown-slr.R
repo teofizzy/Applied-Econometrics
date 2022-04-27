4a2ee98e04b364726ba249badf557dde6edea237
Teofilo Acholla Ligawa Gafna <teofilo48ligawa@gmail.com>
2022-04-27 23:21:45 +0300

    granger1

--- collegetown-slr.R
+++ collegetown-slr.R
@@ -22,5 +22,8 @@ geom_line(plotly)
 ggplot(X1649256667723_collegetown, aes(x = sqft, y = price)) +
         geom_point(shape = 19) +
         geom_smooth(method = "lm", se = FALSE, colour = "red")
+
+## conducting a granger causality test, first granger then reverse granger
 library(lmtest)
 grangertest(price ~ sqft, order = 3, data = X1649256667723_collegetown)
+grangertest(sqft ~ price, order = 3, data = X1649256667723_collegetown)
