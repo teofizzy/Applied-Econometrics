551aa7dec253e86135f730fcb07097f781817838
Teofilo Acholla Ligawa Gafna <teofilo48ligawa@gmail.com>
2022-04-27 12:58:22 +0300

    collegetown slr

---
+++ collegetown-slr.R
@@ -0,0 +1,24 @@
+require(ggplot2)
+require(tidyverse)
+require(broom)
+require(ggpubr)
+X1649256667723_collegetown
+scatter1<-qplot(price,sqft, data = X1649256667723_collegetown)
+scatter1
+slr1<-lm(price~sqft, data = X1649256667723_collegetown)
+slr1
+summary(slr1)
+abline(slr1)
+abline(lm(X1649256667723_collegetown$price~X1649256667723_collegetown$sqft))
+plotly<-plot(X1649256667723_collegetown$price, X1649256667723_collegetown$sqft,
+     pch =20,
+     col = "red",
+     main = "scatterplot of price against square feet",
+     xlab = "Price",
+     ylab = "square feet")
+abline(lm(X1649256667723_collegetown$price~X1649256667723_collegetown$sqft))
+geom_abline(plotly)
+geom_line(plotly)
+ggplot(X1649256667723_collegetown, aes(x = sqft, y = price)) +
+        geom_point(shape = 19) +
+        geom_smooth(method = "lm", se = FALSE, colour = "red")
